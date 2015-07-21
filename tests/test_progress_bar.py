f95066c748a4103daeb785f2ddf774af0b644ddd
MisanthropicBit <alexander.asp.bock@gmail.com>
2015-07-21 18:03:11 +0200

    Fixed flake8 indentation errors occurring after uncommenting

--- tests/test_progress_bar.py
+++ tests/test_progress_bar.py
@@ -71,134 +71,134 @@ def approx_equals(a, b):
 
 
 def test_proper_init():
-   """Test proper initialisation."""
-   testbar = progress.ProgressBar('[{progress}]')
+    """Test proper initialisation."""
+    testbar = progress.ProgressBar('[{progress}]')
 
-   assert testbar.min == 0
-   assert testbar.max == 100
-   assert testbar.char == '='
-   assert testbar.head == '>'
-   assert testbar.width == 20
-   assert len(testbar) == len('[' + (testbar.fill * testbar.width) + ']')
-   assert not testbar._has_eta
-   assert testbar._etaobj is None
+    assert testbar.min == 0
+    assert testbar.max == 100
+    assert testbar.char == '='
+    assert testbar.head == '>'
+    assert testbar.width == 20
+    assert len(testbar) == len('[' + (testbar.fill * testbar.width) + ']')
+    assert not testbar._has_eta
+    assert testbar._etaobj is None
 
 
 def test_progressbar_fail():
-   """Test keyword arguments that should fail."""
-   for kwargs in fail_kwargs:
-       with pytest.raises(ValueError):
-           progress.ProgressBar(**kwargs)
+    """Test keyword arguments that should fail."""
+    for kwargs in fail_kwargs:
+        with pytest.raises(ValueError):
+            progress.ProgressBar(**kwargs)
 
 
 def test_eta_fail():
-   """Test failing ETA objects."""
-   for etaobj in (FailETA1(), FailETA2(), FailETA3()):
-       with pytest.raises(ValueError):
-           progress.ProgressBar(fmt=' ', etaobj=etaobj)
+    """Test failing ETA objects."""
+    for etaobj in (FailETA1(), FailETA2(), FailETA3()):
+        with pytest.raises(ValueError):
+            progress.ProgressBar(fmt=' ', etaobj=etaobj)
 
 
 def test_property_fails():
-   """Test values where properties should fail."""
-   testbar = progress.ProgressBar('[{progress}]')
+    """Test values where properties should fail."""
+    testbar = progress.ProgressBar('[{progress}]')
 
-   with pytest.raises(ValueError):
-       testbar.char = ''
+    with pytest.raises(ValueError):
+        testbar.char = ''
 
-   with pytest.raises(ValueError):
-       testbar.head = ''
+    with pytest.raises(ValueError):
+        testbar.head = ''
 
-   with pytest.raises(ValueError):
-       testbar.fill = ''
+    with pytest.raises(ValueError):
+        testbar.fill = ''
 
-   with pytest.raises(ValueError):
-       testbar.char = '**'
+    with pytest.raises(ValueError):
+        testbar.char = '**'
 
-   with pytest.raises(ValueError):
-       testbar.head = ')))'
+    with pytest.raises(ValueError):
+        testbar.head = ')))'
 
-   with pytest.raises(ValueError):
-       testbar.fill = 'urso'
+    with pytest.raises(ValueError):
+        testbar.fill = 'urso'
 
-   with pytest.raises(ValueError):
-       testbar.width = 0
+    with pytest.raises(ValueError):
+        testbar.width = 0
 
-   with pytest.raises(ValueError):
-       testbar.width = -20
+    with pytest.raises(ValueError):
+        testbar.width = -20
 
-   with pytest.raises(ValueError):
-       testbar.min = 100
+    with pytest.raises(ValueError):
+        testbar.min = 100
 
-   with pytest.raises(ValueError):
-       testbar.min = 101
+    with pytest.raises(ValueError):
+        testbar.min = 101
 
-   with pytest.raises(ValueError):
-       testbar.max = 0
+    with pytest.raises(ValueError):
+        testbar.max = 0
 
-   with pytest.raises(ValueError):
-       testbar.max = -1
+    with pytest.raises(ValueError):
+        testbar.max = -1
 
 
 def test_output_targets():
-   """Test switching targets."""
-   testbar = progress.ProgressBar('[{progress}]')
+    """Test switching targets."""
+    testbar = progress.ProgressBar('[{progress}]')
 
-   testbar.target = sys.stdout
-   assert testbar.target is sys.stdout
+    testbar.target = sys.stdout
+    assert testbar.target is sys.stdout
 
-   with pytest.raises(ValueError):
-       testbar.target = sys.stdin
+    with pytest.raises(ValueError):
+        testbar.target = sys.stdin
 
 
 def test_updates():
-   """Test updating the progress bar."""
-   testbar = progress.ProgressBar('[{progress}]')
+    """Test updating the progress bar."""
+    testbar = progress.ProgressBar('[{progress}]')
 
-   # Attempt to update with a negative value
-   with pytest.raises(ValueError):
-       testbar.update(-10)
+    # Attempt to update with a negative value
+    with pytest.raises(ValueError):
+        testbar.update(-10)
 
-   with pytest.raises(ValueError):
-       testbar += -20
+    with pytest.raises(ValueError):
+        testbar += -20
 
-   # Test updates/states through properties
-   testbar.value = 50
-   assert testbar.value == 50
-   assert testbar.percent == 0.5
-   assert testbar.max == 100
-   assert testbar.target is sys.stderr
+    # Test updates/states through properties
+    testbar.value = 50
+    assert testbar.value == 50
+    assert testbar.percent == 0.5
+    assert testbar.max == 100
+    assert testbar.target is sys.stderr
 
-   testbar.update(25)
-   assert testbar.value == 75
-   assert testbar.percent == 0.75
+    testbar.update(25)
+    assert testbar.value == 75
+    assert testbar.percent == 0.75
 
-   testbar.value = 10
-   assert testbar.value == 10
-   assert testbar.percent == 0.10
+    testbar.value = 10
+    assert testbar.value == 10
+    assert testbar.percent == 0.10
 
-   testbar += 50
-   assert testbar.value == 60
-   assert testbar.percent == 0.60
+    testbar += 50
+    assert testbar.value == 60
+    assert testbar.percent == 0.60
 
-   testbar.value = -10
-   assert testbar.value == 0
-   assert testbar.percent == 0.0
+    testbar.value = -10
+    assert testbar.value == 0
+    assert testbar.percent == 0.0
 
-   testbar.value = 1000
-   assert testbar.value == 100
-   assert testbar.percent == 1.0
-   assert testbar.done()
+    testbar.value = 1000
+    assert testbar.value == 100
+    assert testbar.percent == 1.0
+    assert testbar.done()
 
 
 def test_autoupdate():
-   """Test the autoupdate feature."""
-   testbar = progress.ProgressBar('[{progress}]')
+    """Test the autoupdate feature."""
+    testbar = progress.ProgressBar('[{progress}]')
 
-   testbar.autoupdate(22)
-   assert testbar.value == 22
-   assert testbar.percent == 0.22
-   assert testbar.min == 0
-   assert not testbar.done()
+    testbar.autoupdate(22)
+    assert testbar.value == 22
+    assert testbar.percent == 0.22
+    assert testbar.min == 0
+    assert not testbar.done()
 
 
 def test_reset():
@@ -215,106 +215,106 @@ def test_reset():
 
 
 def test_percent_property():
-   """Test 'percent' property."""
-   testbar = progress.ProgressBar('[{progress}]')
+    """Test 'percent' property."""
+    testbar = progress.ProgressBar('[{progress}]')
 
-   testbar.percent = 0.73408248
-   assert approx_equals(testbar.percent, 0.73408248)
-   assert testbar.value == testbar.max * 0.73408248
-   assert testbar.min == 0
-   assert testbar.max == 100
+    testbar.percent = 0.73408248
+    assert approx_equals(testbar.percent, 0.73408248)
+    assert testbar.value == testbar.max * 0.73408248
+    assert testbar.min == 0
+    assert testbar.max == 100
 
 
 def test_width_property():
-   """Test 'width' property."""
-   testbar = progress.ProgressBar('[{progress}]')
+    """Test 'width' property."""
+    testbar = progress.ProgressBar('[{progress}]')
 
-   testbar.value = 75
-   assert testbar.value == 75
-   testbar.width = 30
-   assert testbar.width == 30
+    testbar.value = 75
+    assert testbar.value == 75
+    testbar.width = 30
+    assert testbar.width == 30
 
-   assert str(testbar) == '[' +\
-       (testbar.char * (int(testbar.width * 0.75) - 1)) + testbar.head +\
-       (testbar.fill * (int(testbar.width * 0.25) + 1)) + ']'
+    assert str(testbar) == '[' +\
+        (testbar.char * (int(testbar.width * 0.75) - 1)) + testbar.head +\
+        (testbar.fill * (int(testbar.width * 0.25) + 1)) + ']'
 
 
 def test_visual_changes():
-   """Test visual feature updates through properties."""
-   testbar = progress.ProgressBar('[{progress}]')
+    """Test visual feature updates through properties."""
+    testbar = progress.ProgressBar('[{progress}]')
 
-   testbar.value = 0
-   testbar += 50
-   testbar.max = 200
-   assert testbar.max == 200
-   assert testbar.min == 0
-   testbar.head = '?'
-   assert testbar.head == '?'
-   testbar.char = '@'
-   assert testbar.char == '@'
-   testbar.fill = '_'
-   assert testbar.fill == '_'
-   assert str(testbar) == '[' +\
-       ('@' * (int(testbar.width * 0.25) - 1)) + '?' +\
-       ('_' * (int(testbar.width * 0.75))) + ']'
-   assert testbar.target is sys.stderr
+    testbar.value = 0
+    testbar += 50
+    testbar.max = 200
+    assert testbar.max == 200
+    assert testbar.min == 0
+    testbar.head = '?'
+    assert testbar.head == '?'
+    testbar.char = '@'
+    assert testbar.char == '@'
+    testbar.fill = '_'
+    assert testbar.fill == '_'
+    assert str(testbar) == '[' +\
+        ('@' * (int(testbar.width * 0.25) - 1)) + '?' +\
+        ('_' * (int(testbar.width * 0.75))) + ']'
+    assert testbar.target is sys.stderr
 
 
 def test_formatting():
-   """Test the 'format' property."""
-   testbar = progress.ProgressBar('[{progress}]')
+    """Test the 'format' property."""
+    testbar = progress.ProgressBar('[{progress}]')
 
-   assert type(testbar.format) is str
-   testbar.format = "{percentage}%"
-   assert testbar.format == "{percentage}%"
+    assert type(testbar.format) is str
+    testbar.format = "{percentage}%"
+    assert testbar.format == "{percentage}%"
 
-   # Test setting an ETA
-   testbar.format = "{progress} {percentage}% {minutes}"
-   assert testbar._has_eta
+    # Test setting an ETA
+    testbar.format = "{progress} {percentage}% {minutes}"
+    assert testbar._has_eta
 
-   # Fails due to 'progress' key being used twice
-   with pytest.raises(ValueError):
-       testbar.format = "{progress} {progress}"
+    # Fails due to 'progress' key being used twice
+    with pytest.raises(ValueError):
+        testbar.format = "{progress} {progress}"
 
-   # Fails due to an empty format string
-   with pytest.raises(ValueError):
-       testbar.format = ""
+    # Fails due to an empty format string
+    with pytest.raises(ValueError):
+        testbar.format = ""
 
 
 def test_min_max_value_properties():
-   """Test the 'min', 'max' and 'value' properties."""
-   testbar = progress.ProgressBar('[{progress}]')
+    """Test the 'min', 'max' and 'value' properties."""
+    testbar = progress.ProgressBar('[{progress}]')
 
-   # Test setting the 'min' property
-   testbar.min = -10
-   assert testbar.min == -10
-   testbar.max = 200
-   assert testbar.max == 200
-   testbar.value = 50
-   assert testbar.value == 50
-   assert approx_equals(testbar.percent, 0.2857142857142857)
+    # Test setting the 'min' property
+    testbar.min = -10
+    assert testbar.min == -10
+    testbar.max = 200
+    assert testbar.max == 200
+    testbar.value = 50
+    assert testbar.value == 50
+    assert approx_equals(testbar.percent, 0.2857142857142857)
 
-   testbar.value = 95
-   assert testbar.value == 95
-   assert approx_equals(testbar.percent, 0.5)
+    testbar.value = 95
+    assert testbar.value == 95
+    assert approx_equals(testbar.percent, 0.5)
 
 
 def test_keyword_updates():
-   """Test keyword arguments in updates."""
-   testbar = progress.ProgressBar('[{progress}]')
+    """Test keyword arguments in updates."""
+    testbar = progress.ProgressBar('[{progress}]')
 
-   l = list(range(3))
-   d = dict(a=1, b=3)
-   testbar.show(*l, **d)
-   testbar.autoupdate(10, *l, **d)
+    l = list(range(3))
+    d = dict(a=1, b=3)
+    testbar.show(*l, **d)
+    testbar.autoupdate(10, *l, **d)
 
 
 def test_reserved_keys():
-   """Test errors on modification of reserved keys."""
-   testbar = progress.ProgressBar('[{progress}]')
-   d = dict()
+    """Test errors on modification of reserved keys."""
+    testbar = progress.ProgressBar('[{progress}]')
+    d = dict()
 
-   # Fails because progress is a reserved key
-   with pytest.raises(ValueError):
-       d['progress'] = 'I am not allowed :('
-       testbar.autoupdate(5, **d)
+    # Fails because progress is a reserved key
+    with pytest.raises(ValueError):
+        d['progress'] = 'I am not allowed :('
+        testbar.autoupdate(5, **d)
