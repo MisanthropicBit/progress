1999c8e0b365a63fd6396a5bbb7b6f0d9d9e9f67
MisanthropicBit <alexander.asp.bock@gmail.com>
2016-10-23 14:28:44 +0200

    Improved download example

--- examples/download.py
+++ examples/download.py
@@ -12,19 +12,19 @@ import progress
 
 def download_file():
     """Simulate downloading a file."""
-    time.sleep(random.uniform(0.1, 0.3))
+    time.sleep(random.uniform(0.1, 0.2))
 
 
 if __name__ == '__main__':
     bar = progress.ProgressBar("Downloading '{}'... ")
 
-    for i in range(5):
+    for i in range(1, 4):
         bar.reset()
         bar.format = "Downloading '{}'... ".format('file' + str(i) + '.txt') +\
             "{nominator}%"
 
         while not bar.done():
-            bar.autoupdate(random.randint(1, 3))
+            bar.autoupdate(random.randint(1, 5))
             download_file()
 
         print()
